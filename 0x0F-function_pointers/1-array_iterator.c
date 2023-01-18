4785b71832b58e723dd6154111852aec05a2751a
sermawutor <leslieashigbi@gmail.com>
2023-01-18 13:09:35 -0800

    win

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,24 +1,19 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
-  * array_iterator - initializes a variable
-  *
-  * @array: name.
-  * @size: pointer.
-  *@action: pointer.
-  * Return: nothing.
-  */
+ * array_iterator - executes a function given as a
+ * parameter on each element of an array.
+ * @array: input integer array.
+ * @size: size of the array.
+ * @action: pointer to the function.
+ *
+ * Return: no return.
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-        size_t i;
+	unsigned int i;
 
-        if (array == NULL || size <= 0 || action == NULL)
-        {
-                return;
-        }
-        for (i = 0; i < size; i++)
-        {
-                (*action)(array[i]);
-        }
+	if (array && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,47 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * print_opcodes - program that prints the opcodes of its own main function.
- * @a: address of the main function
- * @n: number of bytes to print
- * Return: nothing.
- */
-void print_opcodes(char *a, int n)
-{
-        int i;
 
-        for (i = 0; i < n; i++)
-        {
-                printf("%.2hhx", a[i]);
-                if (i < n - 1)
-                {
-                        printf(" ");
-                }
-        }
-        printf("\n");
-}
 /**
- * main - prints the opcodes of its own main function
- * @argc: integer
- * @argv: character
- * Return: always 0.
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-        int ae;
+	char *opc = (char *) main;
+	int i, nbytes;
 
-        if (argc != 2)
-        {
-                printf("Error\n");
-                exit(1);
-        }
-        ae = atoi(argv[1]);
-        if (ae < 0)
-        {
-                printf("Error\n");
-                exit(2);
-        }
-        print_opcodes((char *)&main, ae);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-        return (0);
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
 }
