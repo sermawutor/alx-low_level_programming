59c6a75abde5707d62f8c60b9e59ffcae5714407
sermawutor <leslieashigbi@gmail.com>
2023-01-26 16:46:14 -0800

    win

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: singly linked list.
- * Return: number of elements in the list.
+ * Return: number of elements in the list... LESLIE
  */
 
 size_t print_list(const list_t *h)
